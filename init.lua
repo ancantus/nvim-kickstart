08c09f435459abbfa64661831d20d829592d7e72
Ryan Estep <rmestep@bex.net>
2025-03-12 15:51:59 +1300

    Added svelte / ts lsps

--- init.lua
+++ init.lua
@@ -619,15 +619,19 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
+
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
+        svelte = {
+          filetypes = { 'svelte', 'svelte.ts', 'svelte.js' },
+        },
 
         lua_ls = {
           -- cmd = { ... },
@@ -722,7 +726,7 @@ require('lazy').setup({
     },
   },
 
-  { -- Autocompletion
+  { -- Autocompletiun
     'hrsh7th/nvim-cmp',
     event = 'InsertEnter',
     dependencies = {
