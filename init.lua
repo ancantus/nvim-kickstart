93909571d510308c0b6fbcc93801aadc60d3c0f8
Ryan Estep <rmestep@bex.net>
2025-10-19 18:54:51 +1300

    Added ocaml

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -620,6 +620,7 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         rust_analyzer = {},
+        -- ocamllsp = {},
 
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -680,6 +681,7 @@ require('lazy').setup({
           end,
         },
       }
+      require('lspconfig')['ocamllsp'].setup {}
     end,
   },
 
